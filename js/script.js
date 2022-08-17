59cae219a982fadee52c0fe97f01fbc0b7146db7
weston-bailey <wdbailey87@gmail.com>
2022-08-17 15:32:24 -0700

    adds form

--- js/script.js
+++ js/script.js
@@ -5,10 +5,14 @@ let fToC = document.querySelector('#fToC')
 let cToF = document.querySelector('#cToF')
 let clear = document.querySelector('#clear')
 let result = document.querySelector('#result')
+let fToCForm = document.querySelector('#fToC-form')
+console.log(fToCForm)
 
 // adding event listeners
 
-fToC.addEventListener("click", function(e) {
+fToCForm.addEventListener('submit', function(e) {
+    // prevent the page from refreshing on submission of form
+    e.preventDefault()
     let preConversion = input.value
     let postConversion = (preConversion - 32) * (5/9)
     result.innerText = postConversion
@@ -18,7 +22,20 @@ fToC.addEventListener("click", function(e) {
     } else if (postConversion > 38) {
         document.body.style.backgroundImage = "url('https://media.giphy.com/media/P7JmDW7IkB7TW/giphy.gif')";
     }
+
 })
+
+// fToC.addEventListener("click", function(e) {
+//     let preConversion = input.value
+//     let postConversion = (preConversion - 32) * (5/9)
+//     result.innerText = postConversion
+
+//     if (postConversion <= 0) {
+//         document.body.style.backgroundImage = "url('https://media.giphy.com/media/xTeV7qibUsOYA3mmYw/giphy.gif')";
+//     } else if (postConversion > 38) {
+//         document.body.style.backgroundImage = "url('https://media.giphy.com/media/P7JmDW7IkB7TW/giphy.gif')";
+//     }
+// })
 
 cToF.addEventListener("click", function(e) {
     let preConversion = input.value
